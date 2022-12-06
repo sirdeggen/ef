d187a55fbb89012a7ceee2cebc7b91980b11d28b
Darren Kellenschwiler <deggen@kschw.com>
2022-12-05 21:57:04 -0600

    better looking?
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 
-export default function Page() {
+const curl = `
+curl --request POST \\
+  --url https://ef.xn--nda.network/api \\
+  --header 'Content-Type: application/json' \\
+  --data '{"rawtx": "0200000001b2faffe1e1d3c88f4092f34646c060ea2b6a93acc3010484c747ed4c051c2555080000006a4730440220392bcec91f190ce38db9bf53d03886ab63d9bd24fcf7174e8a8df21d23382ba7022038f20c1f3f6583951d01af0be30612a6c0b46d949b4aae60f42644ce513f3e55412103ea0ff49ec6fbb9cbc942d9c1fce9c04e12a91c1209b239466e0a29147da55db1ffffffff0390010000000000001976a9144d255baa50a14bef4cce1eb8012a02768e8ffaa888acd3600000000000001976a91447e22d8011bb446cc3f606179e333f64a9b6206b88ac04915500000000001976a914d24cb016397008a85c88b1278a36434fdd4e801f88ac00000000"}'
+`
+
+const Page = () => {
     return (
         <div>
             <Head>
@@ -9,13 +16,14 @@ export default function Page() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-            <h1>
-                Extended Format Service API
-            </h1>
-            <p>
-                Send your rawtx to the /api endpoint to get the extended format in the response.
-            </p>
+                <h1>Extended Format Service API</h1>
+                <p>Send your rawtx to the /api endpoint to get the extended format in the response.</p>
+                <code style={{ textAlign: 'left' }}>
+                    <pre>{curl}</pre>
+                </code>
             </main>
         </div>
     )
 }
+
+export default Page
